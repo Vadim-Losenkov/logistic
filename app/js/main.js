9369f9e70827a8809c35c16e509d133009930874
Вадим Лосенков <vProg0729@gmail.com>
2022-04-17 10:39:36 +0300

    added popups

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,126 @@
 $(function () {
+  $('.open-popup').magnificPopup({
+    type: 'inline',
+    removalDelay: 500, //delay removal by X to allow out-animation
+    callbacks: {
+      beforeOpen: function () {
+        this.st.mainClass = this.st.el.attr('data-effect');
+      },
+    },
+    midClick: true, // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+  });
+  $('[data-popup="close"]').on('click', (e) => {
+    $('.open-popup').magnificPopup('close');
+    e.preventDefault();
+  });
+  $('.user-popup__info-input').focusin(function () {
+    $(this).siblings('svg').children('path').css({
+      fill: '#000',
+      transition: '.3s',
+    });
+  });
+  $('.user-popup__info-input').focusout(function () {
+    $(this).siblings('svg').children('path').css({
+      fill: '#ccc',
+      transition: '.3s',
+    });
+  });
+  if ($('[data-popup="form-up"]').length && $('[data-popup="form-in"]')) {
+    $('[data-popup="form-up"]').validate({
+      errorClass: 'invalid-field',
+      rules: {
+        email: {
+          required: true,
+          email: true,
+        },
+        password: {
+          required: true,
+          minlength: 5,
+        },
+        name: {
+          required: true,
+          minlength: 2,
+        },
+        sname: {
+          required: true,
+          minlength: 2,
+        },
+      },
+      messages: {
+        password: {
+          required: `<p class="input-error">Введите пароль</p>`,
+          minlength: jQuery.validator.format(`<p class="input-error">Минимум {0} символов</p>`),
+        },
+        name: {
+          required: `<p class="input-error">Введите имя</p>`,
+          minlength: jQuery.validator.format(`<p class="input-error">Минимум {0} символов</p>`),
+        },
+        sname: {
+          required: `<p class="input-error">Введите фамилию</p>`,
+          minlength: jQuery.validator.format(`<p class="input-error">Минимум {0} символов</p>`),
+        },
+        email: {
+          required: `<p class="input-error">Введите E-mail</p>`,
+          email: `<p class="input-error">Введите корректный E-mail</p>`,
+        },
+      },
+      // the errorPlacement has to take the table layout into account
+      errorPlacement: function (error, element) {
+        error.appendTo(element.parent().next());
+      },
+      // specifying a submitHandler prevents the default submit, good for the demo
+      submitHandler: function (e) {
+        $('.open-popup').magnificPopup('close');
+      },
+      // set this class to error-labels to indicate valid fields
+      success: function (element) {
+        element.html(`
+          <svg class="input-error-icon" width="15" height="11" viewBox="0 0 15 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M15 1.15481L4.8 11L0 6.36697L1.19642 5.21216L4.8 8.6822L13.8036 0L15 1.15481Z" fill="#22BC5F"/>
+          </svg>
+        `);
+      },
+    });
+    $('[data-popup="form-in"]').validate({
+      errorClass: 'invalid-field',
+      rules: {
+        email: {
+          required: true,
+          email: true,
+        },
+        password: {
+          required: true,
+          minlength: 5,
+        },
+      },
+      messages: {
+        password: {
+          required: `<p class="input-error">Введите пароль</p>`,
+          minlength: jQuery.validator.format(`<p class="input-error">Минимум {0} символов</p>`),
+        },
+        email: {
+          required: `<p class="input-error">Введите E-mail</p>`,
+          email: `<p class="input-error">Введите корректный E-mail</p>`,
+        },
+      },
+      // the errorPlacement has to take the table layout into account
+      errorPlacement: function (error, element) {
+        error.appendTo(element.parent().next());
+      },
+      // specifying a submitHandler prevents the default submit, good for the demo
+      submitHandler: function (e) {
+        $('.open-popup').magnificPopup('close');
+      },
+      // set this class to error-labels to indicate valid fields
+      success: function (element) {
+        element.html(`
+          <svg class="input-error-icon" width="15" height="11" viewBox="0 0 15 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M15 1.15481L4.8 11L0 6.36697L1.19642 5.21216L4.8 8.6822L13.8036 0L15 1.15481Z" fill="#22BC5F"/>
+          </svg>
+        `);
+      },
+    });
+  }
   $('[data-button="toSecton"]').on('click', function (e) {
     e.preventDefault();
     const sectionID = $(this).attr('href');
